8f4515295345aa3735d0b309f402bb13f30eff3d
Alessandro <alessandrodess@gmail.com>
2025-04-14 17:31:04 +0200

    init: sanity inside nextjs project

---
+++ src/app/dashboard/config/sanity.config.ts
@@ -0,0 +1,10 @@
+import { defineConfig } from 'sanity';
+
+export default defineConfig({
+  name: 'default',
+  title: 'Portfolio Dashboard',
+  projectId: 'w4peuxk7',
+  dataset: 'production',
+  basePath: '/dashboard',
+  schema: {},
+});
